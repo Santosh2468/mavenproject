91946e0470f80c39e459ac61f21d23eadcdd7201
Santosh2468 <104915559+Santosh2468@users.noreply.github.com>
2023-01-23 20:15:47 +0530

    Update Employee_Question_Statement3.java

--- Employee_Question_Statement3.java
+++ Employee_Question_Statement3.java
@@ -1,37 +1,26 @@
-    import java.util.Scanner;
-    public class Employee_Question_Statement3 {
-        public static void main(String[] args) {
-            Scanner inp = new Scanner(System.in);
-            System.out.println("Enter number of employees : ");
-
-            int len = inp.nextInt();
-            int[] regno = new int[len];
-            String[] empName = new String[len];
-            double[] sal = new double[len];
-            for(int i = 0; i < len; i++){
-
-                System.out.println("Enter employee id: ");
-                regno[i] = inp.nextInt();
-                System.out.println("Enter employee name: ");
-                empName[i] = inp.next();
-                System.out.println("Enter employee salary: ");
-                sal[i] = inp.nextDouble();
-
-            }
-            System.out.println("Enter name to search from available data: ");
-            String name = inp.next();
-            display(name, regno, empName, sal);
-        }
-
-        public static void display(String name,int regno[],String Empname[],double salary[]){
-            //loop to search iteam
-            for(int i = 0; i < Empname.length; i++){
-                if(name.equalsIgnoreCase(Empname[i])){
-                    System.out.println("ID \t Name \t Salary");
-                    System.out.println("ID "+regno[i]+"\t "+Empname[i]+" \t"+salary[i]);
-                    break;
-                }
-            }
-        }
-    }
-
+def add(a,b):
+     return a+b
+ 
+def sub(a,b):
+     return a-b
+ 
+def prod(a,b):
+     return a * b
+ 
+def div(a,b):
+     return a / b
+ 
+def si(p, r, t):
+    return (p*r*t) / 100
+ 
+def ci(p, r ,t):
+    return p * pow((1 + r/100), t)
+ 
+def sqr(num):
+    return num**2
+ 
+def sqrt(num)
+    return num**0.5
+ 
+print(add(10,15))
+#to add two numbers, similarly you can use other functions for other operations.
